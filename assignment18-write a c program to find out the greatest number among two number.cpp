0b28085f9fa937705820fac1b3ee5406c2d8a9db
Bidisha-das <djuthika94@gmail.com>
2025-01-27 03:21:07 -1200

    Add files via upload

---
+++ assignment18-write a c program to find out the greatest number among two number.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2;
+	num1=6,num2=4;
+	if(num1==num2)
+	printf("both are equal");
+	else if(num1>num2)
+	printf("%d is greater",num1);
+	else
+	printf("%d is greater",num2);
+	return 0;
+}
